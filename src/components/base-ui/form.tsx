842d35c8627be84b2640aeee7fb65bc4be659b66
Bruno Azevedo <brunoacev@gmail.com>
2024-02-09 17:09:48 -0300

    Fix: Change color for dark mode on form

--- src/components/base-ui/form.tsx
+++ src/components/base-ui/form.tsx
@@ -10,7 +10,7 @@ export const Form = ({ className, children, ...rest }: Form) => {
   return (
     <form
       className={cn(
-        "bg-zinc-50 dark:bg-zinc-950/50 px-4 py-6 rounded-md shadow-md flex flex-col gap-4 w-full max-w-md items-start",
+        "bg-zinc-50 dark:bg-zinc-700/50 px-4 py-6 rounded-md shadow-md flex flex-col gap-4 w-full max-w-md items-start",
         className
       )}
       {...rest}
